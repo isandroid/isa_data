c8371e49425d1b9d1b7d173457de81380a61745d
isandroid <isa.mujahid.islam@gmail.com>
2022-06-20 21:44:43 +0700

    menambahkan kolom tanggal pada pengobatan homeopati

--- models/homeopati_pengobatan.py
+++ models/homeopati_pengobatan.py
@@ -6,11 +6,12 @@ class HomeopatiPengobatan(models.Model):
     _name = 'isa_data.homeopati_pengobatan'
     _description = 'Data Pengobatan Homeopati'
 
+    tanggal = fields.Date(string='Tanggal', required=True)
     pasien_nama = fields.Many2one('isa_data.homeopati_pasien', string='Nama Pasien', required=True)
     pasien_aims = fields.Integer(related='pasien_nama.aims', string='AIMS', store=True)
     pasien_jemaat = fields.Many2one(related='pasien_nama.jemaat', string='Jemaat', store=True)
     keluhan = fields.Text(string='Keluhan', required=True)
-    remedi = fields.Many2many('isa_data.homeopati_remedi', string='Remedi', required=True)
+    remedi = fields.Many2many('isa_data.homeopati_remedi', string='Remedi')
 
     # def name_get(self):
     #     return self.mapped(lambda it: (it.id, f"{it.nama} - {it.jemaat}"))
