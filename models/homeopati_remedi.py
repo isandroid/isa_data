d35b66f8e80ee1531079d8f6252fdb9479e904d6
isandroid <isa.mujahid.islam@gmail.com>
2022-07-05 21:54:22 +0700

    update remedi

--- models/homeopati_remedi.py
+++ models/homeopati_remedi.py
@@ -7,8 +7,24 @@ class HomeopatiRemedi(models.Model):
     _description = 'Data Remedi Homeopati'
 
     remedi = fields.Char(string='Remedi', required=True)
+    name = fields.Char(compute='_compute_name', string='Remedi 2', required=True, oldname='remedi', store=True)
     potensi = fields.Char(string='Potensi')
     tautan = fields.Char(string='Tautan')
 
-    def name_get(self):
-        return self.mapped(lambda it: (it.id, f"{it.remedi} - {it.potensi}"))
+    # def name_get(self):
+        # return self.mapped(lambda it: (it.id, f"{it.remedi} - {it.potensi}"))
+
+    # @api.multi
+    # def name_get(self):
+    #     result = []
+    #     for me_id in self :
+    #         result.append((me_id.id, "%s"%(me_id.remedi)))
+    #     return result
+
+    @api.depends('remedi', 'potensi')
+    def _compute_name(self):
+        for me_id in self :
+            if me_id.potensi:
+                me_id.name = f"{me_id.remedi} - {me_id.potensi}"
+            else:
+                me_id.name = f"{me_id.remedi}"
